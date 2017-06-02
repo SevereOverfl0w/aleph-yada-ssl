3520504a38ca95bc477b42a3527bfc015f9d43df
Dominic Monroe <dominic@juxt.pro>
2017-06-02 10:13:08 +0100

    Initial commit with aleph

---
+++ build.boot
@@ -0,0 +1,5 @@
+(set-env!
+  :resource-paths #{"src"}
+  :dependencies '[[aleph "0.4.3"]
+                  [yada "1.2.4"]
+                  [mount "0.1.11"]])
---
+++ src/jkdjka/aleph.clj
@@ -0,0 +1,13 @@
+(ns jkdjka.aleph
+  (:require
+    [aleph.http :as http]
+    [mount.core :refer [defstate]]))
+
+(defn handler [req]
+  {:status 200
+   :headers {"content-type" "text/plain"}
+   :body "hello from aleph!"})
+
+(defstate aleph-server
+  :start (def x (http/start-server handler {:port 8080}))
+  :stop (.close x))
